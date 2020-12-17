bfee909d9b9485adc99f8d2bbbeef0a889189d58
Diego Alejandro Arévalo <33522594+daag2197@users.noreply.github.com>
2020-12-17 17:24:09 -0600

    Header
    Quitar token

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -16,7 +16,7 @@ export class SpotifyService {
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQBkR9aoLQ7Rho4lwSYZy07ggkdou9iUhXwZaHMW-Y3A2TwZp59bM9UZNDz196367H4QH6t-ELyK7WwRMB0'
+      Authorization: 'Bearer Aqui va el token'
     });
 
     return this.http.get(url, { headers });
